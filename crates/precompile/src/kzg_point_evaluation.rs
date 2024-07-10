0fe95e83a13e5ef9db2f5b5f522837a09cca7d46
Dan Cline <6798349+Rjected@users.noreply.github.com>
2024-07-10 16:22:59 -0400

    add c-kzg based implementation for fuzzing

--- crates/precompile/src/kzg_point_evaluation.rs
+++ crates/precompile/src/kzg_point_evaluation.rs
@@ -99,6 +99,111 @@ pub fn as_bytes48(bytes: &[u8]) -> &Bytes48 {
     unsafe { &*as_array::<48>(bytes).as_ptr().cast() }
 }
 
+#[cfg(feature = "kzg-rs")]
+pub mod c_kzg_precompile {
+    use crate::{Address, Error, PrecompileResult};
+    use c_kzg::{Bytes32, Bytes48, KzgProof, KzgSettings};
+    use once_cell::race::OnceBox;
+    use revm_primitives::kzg::{G1_POINTS, G2_POINTS};
+    use revm_primitives::{hex_literal::hex, Bytes, PrecompileOutput};
+    use sha2::{Digest, Sha256};
+
+    static DEFAULT: OnceBox<KzgSettings> = OnceBox::new();
+
+    /// Run kzg point evaluation precompile.
+    ///
+    /// The Env has the KZGSettings that is needed for evaluation.
+    ///
+    /// The input is encoded as follows:
+    /// | versioned_hash |  z  |  y  | commitment | proof |
+    /// |     32         | 32  | 32  |     48     |   48  |
+    /// with z and y being padded 32 byte big endian values
+    pub fn run(input: &Bytes, gas_limit: u64) -> PrecompileResult {
+        if gas_limit < GAS_COST {
+            return Err(Error::OutOfGas.into());
+        }
+
+        // Verify input length.
+        if input.len() != 192 {
+            return Err(Error::BlobInvalidInputLength.into());
+        }
+
+        // Verify commitment matches versioned_hash
+        let versioned_hash = &input[..32];
+        let commitment = &input[96..144];
+        if kzg_to_versioned_hash(commitment) != versioned_hash {
+            return Err(Error::BlobMismatchedVersion.into());
+        }
+
+        DEFAULT.get_or_init(|| {
+            let settings = KzgSettings::load_trusted_setup(G1_POINTS.as_ref(), G2_POINTS.as_ref())
+                .expect("failed to load default trusted setup");
+            Box::new(settings)
+        });
+
+        // Verify KZG proof with z and y in big endian format
+        let commitment = as_bytes48(commitment);
+        let z = as_bytes32(&input[32..64]);
+        let y = as_bytes32(&input[64..96]);
+        let proof = as_bytes48(&input[144..192]);
+        if !verify_kzg_proof(commitment, z, y, proof, DEFAULT.get().unwrap()) {
+            return Err(Error::BlobVerifyKzgProofFailed.into());
+        }
+
+        // Return FIELD_ELEMENTS_PER_BLOB and BLS_MODULUS as padded 32 byte big endian values
+        Ok(PrecompileOutput::new(GAS_COST, RETURN_VALUE.into()))
+    }
+
+    pub const ADDRESS: Address = crate::u64_to_address(0x0A);
+    pub const GAS_COST: u64 = 50_000;
+    pub const VERSIONED_HASH_VERSION_KZG: u8 = 0x01;
+
+    /// `U256(FIELD_ELEMENTS_PER_BLOB).to_be_bytes() ++ BLS_MODULUS.to_bytes32()`
+    pub const RETURN_VALUE: &[u8; 64] = &hex!(
+        "0000000000000000000000000000000000000000000000000000000000001000"
+        "73eda753299d7d483339d80809a1d80553bda402fffe5bfeffffffff00000001"
+    );
+
+    /// `VERSIONED_HASH_VERSION_KZG ++ sha256(commitment)[1..]`
+    #[inline]
+    pub fn kzg_to_versioned_hash(commitment: &[u8]) -> [u8; 32] {
+        let mut hash: [u8; 32] = Sha256::digest(commitment).into();
+        hash[0] = VERSIONED_HASH_VERSION_KZG;
+        hash
+    }
+
+    #[inline]
+    pub fn verify_kzg_proof(
+        commitment: &Bytes48,
+        z: &Bytes32,
+        y: &Bytes32,
+        proof: &Bytes48,
+        kzg_settings: &KzgSettings,
+    ) -> bool {
+        KzgProof::verify_kzg_proof(commitment, z, y, proof, kzg_settings).unwrap_or(false)
+    }
+
+    #[inline]
+    #[track_caller]
+    pub fn as_array<const N: usize>(bytes: &[u8]) -> &[u8; N] {
+        bytes.try_into().expect("slice with incorrect length")
+    }
+
+    #[inline]
+    #[track_caller]
+    pub fn as_bytes32(bytes: &[u8]) -> &Bytes32 {
+        // SAFETY: `#[repr(C)] Bytes32([u8; 32])`
+        unsafe { &*as_array::<32>(bytes).as_ptr().cast() }
+    }
+
+    #[inline]
+    #[track_caller]
+    pub fn as_bytes48(bytes: &[u8]) -> &Bytes48 {
+        // SAFETY: `#[repr(C)] Bytes48([u8; 48])`
+        unsafe { &*as_array::<48>(bytes).as_ptr().cast() }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
